8f10a6dcdadb32686b4e5f8f2288153c8b648a1b
Andriy Dmytrovych Manin <156002628+andrejmanin@users.noreply.github.com>
2024-05-15 19:20:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -45,7 +45,6 @@ void task_4() {
     int min = 0, max = 0;
     for(int i = 0; i < size; i++) {
         cin >> arr[i];
-        cout << arr[i] << endl;
     }
     min = arr[0];
     max = arr[0];
